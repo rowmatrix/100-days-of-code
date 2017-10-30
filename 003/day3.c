600c57b4a79f301ed68141c8b32c9b03282fbb15
rowmatrix <romayibar@gmail.com>
2017-10-29 17:55:27 -0700

    Fixed day3.c

--- 003/day3.c
+++ 003/day3.c
@@ -4,23 +4,29 @@ int main()
 {
     int num = 0;
 
+    printf("Please enter a positive integer: "); 
+    scanf("%d", &num);
+    
     while (num != -1) {
-        printf("Please enter a positive integer: "); 
-        scanf("%d", &num);
-
         if (num < 0 && num != -1) {
-            printf("%d is not a positive integer\n", num);
+            printf("%d is not a positive integer\n\n", num);
         } 
         else {
             int i;
+            int flag = 0;
             for (i = num-1; i >= 2; i--) {
                 if (num % i == 0) {
                     printf("%d COMPOSITE %d\n\n", num, i);
+                    flag = 1;
                     break;
                 }
+            }
+            if (!flag) {
                 printf("%d PRIME\n\n", num); /* <<< Change location! */
             }
         }
+        printf("Please enter a positive integer: "); 
+        scanf("%d", &num);
     }
 
     printf("Thank you for using my program.\n");
